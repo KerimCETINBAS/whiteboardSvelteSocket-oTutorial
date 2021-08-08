c367f8945c4c1df892c3e296bda6393ef49f0e52
kerimCetinbas <a.kerim.cetinbas@yandex.com>
2021-08-09 01:20:55 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte'
+	import Landing from './Landing.svelte'
+	import Drawing from './Drawing.svelte'
+	
+	export let isInSession 
+
+
+
+
+
+</script>
+
+
+
+
+{#if isInSession } 
+	<svelte:component bind:isInSession  this={Drawing}  />
+{:else}
+	<svelte:component bind:isInSession   this={Landing}  />
+{/if}
